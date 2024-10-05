d0351ae0df12152ef80257ba4692bff995ead4a5
KamogeloMahlake <3949843@myuwc.ac.za>
2024-10-05 19:48:23 +0200

    reworking the code

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
     if (x === 0) {
@@ -9,60 +12,67 @@ function getComputerChoice() {
     }    
 }
 
-function getHumanChoice() {
-    let x = prompt("Enter rock, paper or scissors").toLowerCase();
-    if (x === "rock" || x === "scissors" || x === "paper") {
-        return x;
-    }  else {
-        alert("Incorrect input");
-        return getHumanChoice();
-    }
-}
-
 function playRound(humanSelection, computerSelection) {
-    if ((humanSelection === "rock"  && computerSelection === "scissors") || (humanSelection === "paper"  && computerSelection === "rock") || (humanSelection === "scissors"  && computerSelection === "paper")) {
-        return "You win"
-    }  else if (humanSelection === computerSelection) {
-        return "Tie"
-    } else {
-        return "You lose"
+    if ((humanSelection === "rock"  && computerSelection === "scissors") 
+    || (humanSelection === "paper"  && computerSelection === "rock") 
+    || (humanSelection === "scissors"  && computerSelection === "paper")) 
+    {
+        humanScore++;
+        text.textContent = `${humanSelection} beats ${computerSelection}\n
+        humanscore: ${humanScore}\n
+        computerscore: ${computerScore}`;
+    }  
+    
+    else if (humanSelection === computerSelection) 
+    {   text.textContent = `It's a tie\n
+        humanscore: ${humanScore}\n
+        computerscore: ${computerScore}`;
     } 
+    else 
+    {   computerScore++;
+        text.textContent = `${computerSelection} beats ${humanSelection}\n
+        humanscore: ${humanScore}\n
+        computerscore: ${computerScore}`;
+    }
+
+    if (humanScore === 5) {
+        text.textContent = `You win\nhumanscore: ${humanScore}
+        \ncomputerscore: ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5 ) {
+        text.textContent = `You lose\nhumanscore: ${humanScore}
+        \ncomputerscore: ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;   
+    }
 
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+const div = document.querySelector("#main");
 
-    for (let i = 0; i < 5;  i++) {
-        let x = getHumanChoice();
-        let y = getComputerChoice();
-        let z = playRound(x, y);
-        if (z === "You win") {
-            humanScore++;
-            console.log(`You win ${x} beats ${y}\nCurrentscore: ${humanScore}\nComputerscore: ${computerScore}`);
-            
-        } else if (z === "You lose") {
-            computerScore++;
-            console.log(`You lose ${y} beats ${x}\nCurrentscore: ${humanScore}\nComputerscore: ${computerScore}`)
+const rock = document.createElement("button");
+rock.textContent = "ROCK";
 
-        } else {
-            computerScore++;
-            humanScore++;
-            console.log(`Draw\nCurrentscore: ${humanScore}\nComputerscore: ${computerScore}`);
-            
-        }
+const paper = document.createElement("button");
+paper.textContent = "PAPER";
 
-    }
+const scissors = document.createElement("button");
+scissors.textContent = "SCISSORS"
 
-    if (humanScore > computerScore) {
-        return `You Won\nfinal score: ${humanScore}\ncomputerscore: ${computerScore}`
-    } else if (humanScore < computerScore) {
-        return `You lost\nfinal score: ${humanScore}\ncomputerscore: ${computerScore}`
-    } else {
-        return `Tie\nfinal score: ${humanScore}\ncomputerscore: ${computerScore}`
-    }
-}
+const text = document.createElement("div");
+const buttons = document.createElement("div");
+text.textContent ="Let the game begin"
 
-console.log(playGame());
+buttons.appendChild(rock);
+buttons.appendChild(paper);
+buttons.appendChild(scissors)
+div.appendChild(buttons)
+div.appendChild(text);
 
+buttons.style.cssText = "display: flex; align-items: center ; justify-content: space-evenly;"
+buttons.style.height = "100px"
+
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
